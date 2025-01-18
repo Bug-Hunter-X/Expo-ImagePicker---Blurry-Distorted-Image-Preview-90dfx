0b49dfb41e97627666fbca3bc03df72e87ad0015
BugHunter-X <askzenai@gmail.com>
2025-01-18 22:50:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+This bug occurs when using the Expo ImagePicker library.  The image selected by the user is not displayed correctly in the preview.  The image appears blurry, distorted, or completely blank.
+
+```javascript
+import * as ImagePicker from 'expo-image-picker';
+
+const pickImage = async () => {
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+  });
+
+  console.log(result);
+
+  if (!result.cancelled) {
+    setImage(result.uri);
+  }
+};
+```
